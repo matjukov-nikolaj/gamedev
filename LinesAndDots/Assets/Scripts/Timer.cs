ce86c7d589aec0f9af79b773b9e701699522eddf
Vovkarik <solekcsgo@gmail.com>
2019-04-18 12:44:12 +0300

    timer version

---
+++ LinesAndDots/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float timeLeft = 5.0f;
+    public Slider timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(GameObject.Find("Slider").GetComponent<Slider>().value);
+        GameObject.Find("Slider").GetComponent<Slider>().value = timeLeft;
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+}
