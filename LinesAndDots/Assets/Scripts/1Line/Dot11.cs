60e77b5315c15e835e35f179ac6bf652e3e1647b
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2019-05-11 17:24:30 +0300

    for vova

---
+++ LinesAndDots/Assets/Scripts/1Line/Dot11.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class Dot11 : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    private bool isActive;
+
+    private Vector3 prevPos;
+
+    private Vector3 currPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPos = transform.position;
+        lineRenderer = this.GetComponent(typeof(LineRenderer)) as LineRenderer;
+        lineRenderer.SetWidth(3, 3);
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = GetCurrentMousePosition().GetValueOrDefault();
+            pos.z = 0;
+            currPos = pos;
+            lineRenderer.SetPosition(1, currPos);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.position != transform.position)
+        {
+            
+        }
+
+        if (isActive)
+        {
+            return;
+        }
+        prevPos = other.transform.position;
+
+        prevPos = currPos;
+        isActive = true;
+    }
+
+    private Vector3? GetCurrentMousePosition()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var plane = new Plane(Vector3.forward, Vector3.zero);
+
+        float rayDistance;
+        if (plane.Raycast(ray, out rayDistance))
+        {
+            Vector3 pos = ray.GetPoint(rayDistance);
+            pos.z = 0;
+            return pos;
+        }
+
+        return null;
+    }
+}
---
+++ LinesAndDots/Assets/Scripts/Dots.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Dots
+    {
+        private List<GameObject> dots = new List<GameObject>();
+
+        public Dots()
+        {
+//            dots.Add(GameObject.Find("Dot11"));
+//            dots.Add(GameObject.Find("Dot21"));
+//            dots.Add(GameObject.Find("Dot31"));
+//            dots.Add(GameObject.Find("Dot41"));
+//            dots.Add(GameObject.Find("Dot12"));
+//            dots.Add(GameObject.Find("Dot22"));
+//            dots.Add(GameObject.Find("Dot32"));
+//            dots.Add(GameObject.Find("Dot42"));
+//            dots.Add(GameObject.Find("Dot13"));
+//            dots.Add(GameObject.Find("Dot23"));
+//            dots.Add(GameObject.Find("Dot33"));
+//            dots.Add(GameObject.Find("Dot43"));
+//            dots.Add(GameObject.Find("Dot14"));
+//            dots.Add(GameObject.Find("Dot24"));
+//            dots.Add(GameObject.Find("Dot34"));
+//            dots.Add(GameObject.Find("Dot44"));
+//            dots.Add(GameObject.Find("Dot15"));
+//            dots.Add(GameObject.Find("Dot25"));
+//            dots.Add(GameObject.Find("Dot35"));
+//            dots.Add(GameObject.Find("Dot45"));
+//            dots.Add(GameObject.Find("Dot16"));
+//            dots.Add(GameObject.Find("Dot26"));
+//            dots.Add(GameObject.Find("Dot36"));
+//            dots.Add(GameObject.Find("Dot46"));
+            
+        }
+
+        public List<GameObject> getDots()
+        {
+            return dots;
+        }
+
+    }
+}
